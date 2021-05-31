02e0dbf41b077ce0c5fb2e852113255f0a7d0274
Kartik Sharma <ks2109kartik@gmail.com>
2021-05-31 12:02:43 +0530

    Added videoplayer functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import Dashboard from './components/Dashboard/Dashboard';
 import SignIn from './components/form/SignIn/SignIn';
 import SignUp from './components/form/SignUp/SignUp';
 import Upload from './components/Upload/Upload';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 function App() {
   return (
     <>
      <Route exact path="/" component={Dashboard}/>
-    <Route exact path="/signIn" component={SignIn}/>
+     <Route exact path="/video/:videoTitle" component={VideoPlayer}/>
+      <Route exact path="/signIn" component={SignIn}/>
       <Route exact path="/signUp" component={SignUp}/>
       <Route exact path="/upload" component={Upload}/> 
       </>
