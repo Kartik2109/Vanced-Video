7a54283a077daeda7d3b4168b84568ec39220e6a
Kartik Sharma <ks2109kartik@gmail.com>
2021-05-31 11:42:55 +0530

    added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
 import SignIn from './components/form/SignIn/SignIn';
 import SignUp from './components/form/SignUp/SignUp';
 import Upload from './components/Upload/Upload';
 function App() {
   return (
     <>
+     <Route exact path="/" component={Dashboard}/>
     <Route exact path="/signIn" component={SignIn}/>
       <Route exact path="/signUp" component={SignUp}/>
-      <Route exact path="/upload" component={Upload}/>
-      
+      <Route exact path="/upload" component={Upload}/> 
       </>
   );
 }
